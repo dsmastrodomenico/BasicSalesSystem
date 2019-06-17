3904c70f8961165d218f1be2faf2cfda9821717b
dsmastrodomenico <darwin.mastrodomenico@gmail.com>
2019-06-16 21:26:29 -0500

    Adding and structuring functions in a basic way for the creation and listing of clients.

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 
+
+clients = ''
+
+
+def create_client(client_name):
+    global clients
+    clients += client_name
+    _add_coma()
+
+
+def _add_coma():
+    global clients
+    clients += ','
+
+
+def list_client():
+    global clients
+    print(clients)
+
+
 if __name__ == '__main__':
-    client = ''
-    client += 'darwin,'
-    client += 'daniel,'
-    print(client)
+    create_client('darwin')
+    create_client('daniel')
+    list_client()
     
