d2b6614ab0950a5e78341d78250a383b32882b15
dsmastrodomenico <darwin.mastrodomenico@gmail.com>
2019-06-16 21:51:15 -0500

    Added a function to add a menu of options, it is validated if the name of the client exists previously.

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 
 
-clients = ''
+clients = 'Ramiro, Daniel, Jose, '
 
 
 def create_client(client_name):
     global clients
-    clients += client_name
-    _add_coma()
+    if client_name not in clients:
+        clients += client_name
+        _add_coma()
+    else:
+        print('Cliente already is in the client\'s list!.')
 
 
 def _add_coma():
     global clients
-    clients += ','
+    clients += ', '
 
 
 def list_client():
@@ -19,8 +22,26 @@ def list_client():
     print(clients)
 
 
+def _print_welcome():
+    print('WELCOME TO PLATZI VENTAS\n' + ('*' * 50))
+    print('What do you like to do today?')
+    print('[C]reate client.')
+    print('[D]elete client.')
+    print('[L]ist clients')
+
+
 if __name__ == '__main__':
-    create_client('darwin')
-    create_client('daniel')
-    list_client()
+    _print_welcome()
+
+    command = input('Option:\t')
+    if command == 'C':
+        client_name = input('What is the client name?.\n')
+        create_client(client_name)
+    elif command == 'D':
+        pass
+    elif command == 'L':
+        list_client()
+    else:
+        print('Invalid command.')
+    
     
