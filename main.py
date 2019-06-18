2b4b618e8de366f203e104e909f12e6d8e287182
dsmastrodomenico <darwin.mastrodomenico@gmail.com>
2019-06-18 02:32:38 -0500

    Added a function to update client names, functions were defined for data entry and error message response.

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 
 clients = ['Ramiro', 'Daniel', 'Jose']
 
-
 def create_client(client_name):
     global clients
     if client_name not in clients:
@@ -10,15 +9,26 @@ def create_client(client_name):
         # _add_coma()
         clients.append(client_name)
     else:
-        print('Cliente already is in the client\'s list!.')
+        _message_error(1)
 
 
 def delete_client(client_name):
     global clients
-    if client_name not in clients:
+    if client_name in clients:
         clients.remove(client_name)
     else:
-        print('Cliente isn\'t in the client\'s list!.')
+        _message_error(2)
+
+
+def update_client(client_name, new_client_name):
+    global clients
+    if client_name in clients:
+        # clients = clients.replace(client_name, new_client_name)
+        pos = clients.index(client_name) 
+        clients.pop(pos)
+        clients.insert(pos, new_client_name)
+    else:
+        _message_error(2)
 
 
 def _add_coma():
@@ -36,24 +46,48 @@ def _print_welcome():
     print('What do you like to do today?')
     print('[C]reate client.')
     print('[D]elete client.')
+    print('[U]pdate client.')
     print('[L]ist clients')
     print('[E]xit')
 
 
+def _message_error(num_message):
+    if num_message == 1:
+        return print('Cliente already is in the client\'s list!.')
+    elif  num_message == 2:
+        return print('Client isn\'t in the client\'s list!.')
+
+
+def _get_cliente_name():
+    return input('What is the client name?.\n')
+
+
 if __name__ == '__main__':
     _print_welcome()
+
     while True:
         command = input('Option:\t')
+        command = command.upper()
+        
         if command == 'C':
-            client_name = input('What is the client name?.\n')
+            client_name = _get_cliente_name()
             create_client(client_name)
+
         elif command == 'D':
-            client_name = input('What is the client name?.\n')
+            client_name = _get_cliente_name()
             delete_client(client_name)
+
+        elif command == 'U':
+            client_name = _get_cliente_name()
+            update_client_name = input('Please, Write the new client name:\n')
+            update_client(client_name, update_client_name)
+
         elif command == 'L':
             list_client()
+
         elif command == 'E':
             break
+
         else:
             print('Invalid command.')
         
