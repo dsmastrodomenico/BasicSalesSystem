18323547d4f5302bb97befd266e0720231d89566
dsmastrodomenico <darwin.mastrodomenico@gmail.com>
2019-06-18 03:15:18 -0500

    redistributing the code, implemented a function for the verification of the existence of the client, including in a formal way the message function in a general way.

--- main.py
+++ main.py
@@ -2,33 +2,41 @@
 
 clients = ['Ramiro', 'Daniel', 'Jose']
 
+
 def create_client(client_name):
     global clients
-    if client_name not in clients:
+    if client_exists(client_name):
+        _message_system(1)
+    else:
         # clients += client_name
         # _add_coma()
         clients.append(client_name)
-    else:
-        _message_error(1)
-
+        
 
 def delete_client(client_name):
     global clients
-    if client_name in clients:
+    if client_exists(client_name):
         clients.remove(client_name)
     else:
-        _message_error(2)
+        _message_system(2)
 
 
 def update_client(client_name, new_client_name):
     global clients
-    if client_name in clients:
+    if client_exists(client_name):
         # clients = clients.replace(client_name, new_client_name)
         pos = clients.index(client_name) 
         clients.pop(pos)
         clients.insert(pos, new_client_name)
     else:
-        _message_error(2)
+        _message_system(2)
+
+
+def client_exists(client_name):
+    global clients
+    # if client_name+','in clients:
+    if client_name in clients:
+        return True
 
 
 def _add_coma():
@@ -51,23 +59,26 @@ def _print_welcome():
     print('[E]xit')
 
 
-def _message_error(num_message):
-    if num_message == 1:
-        return print('Cliente already is in the client\'s list!.')
+def _message_system(num_message):
+    if num_message == 0:
+        return print('Thanks for everything.\nHave a nice day.')
+    elif num_message == 1:
+        return print('Client already is in the client\'s list!.')
     elif  num_message == 2:
         return print('Client isn\'t in the client\'s list!.')
+    elif  num_message == 3:
+        return print('Invalid command.')
 
 
 def _get_cliente_name():
-    return input('What is the client name?.\n')
+    return input('What is the client name?.\n').capitalize()
 
 
 if __name__ == '__main__':
     _print_welcome()
 
     while True:
-        command = input('Option:\t')
-        command = command.upper()
+        command = input('Option:\t').upper()
         
         if command == 'C':
             client_name = _get_cliente_name()
@@ -79,16 +90,20 @@ if __name__ == '__main__':
 
         elif command == 'U':
             client_name = _get_cliente_name()
-            update_client_name = input('Please, Write the new client name:\n')
-            update_client(client_name, update_client_name)
+            if client_exists(client_name):
+                update_client_name = input('Please, Write the new client name:\n').capitalize()
+                update_client(client_name, update_client_name)
+            else:
+                _message_system(2)
 
         elif command == 'L':
             list_client()
 
         elif command == 'E':
+            _message_system(0)
             break
 
         else:
-            print('Invalid command.')
+            _message_system(3)
         
     
